41b087c0a315983634c27e8459ca324c95c9cce7
CJT1105 <CJT1105@GMAIL.COM>
2016-07-07 14:31:13 -0500

    added some domfunctions

---
+++ src/DOMfunctions.js
@@ -0,0 +1,33 @@
+
+"use-strict"
+let Characters = require("./Character.js");
+let characterArray = Characters.characterObjects;
+console.log("character",characterArray );
+
+var populateDom = function(){
+	for (let i = 0; i < characterArray.length; i ++){
+	$('#characterContainer').append(`<button class="characterButton">${characterArray[i].characterName}</button>`)
+	$('#charSelectContainer').removeClass('hidden');
+	}
+}
+var addClickListeners = function(){
+	var buttons = document.querySelectorAll('.characterButton');
+	for(let i = 0; i < buttons.length; i ++){
+		buttons[i].addEventListener("click", function(){
+			let test = this.innerHTML;
+			console.log("test",test );
+			for(let i = 0; i < characterArray.length; i ++){
+				let current = characterArray[i];
+				if (test === current.characterName){
+					let user1 = current;
+					console.log("user1",user1 );
+				}
+			}
+		})
+	}
+	return user1
+}
+
+
+
+module.exports = { populateDom, addClickListeners,}
