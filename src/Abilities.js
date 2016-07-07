03420f4473f1d140df8a3b158108af829b2dc816
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-07-07 15:11:43 -0500

    singer/actor abilities

--- src/Abilities.js
+++ src/Abilities.js
@@ -1,69 +1,77 @@
 "use strict";
 
-///// new ability constructor/////
+///// NEW ABILITY CONSTRUCTOR/////
 let newAbility = function() {
 	this.ability = "Bare Hands";
 	this.damage = 2;
+	this.type = null;
 };
 
-///// other abilities/////
 
+///// ABILITY TYPES///////
+let actorAbility = function() {
+	this.type = actor;
+}
+actorAbility.prototype = newAbility();
+
+let singerAbility = function() {
+	this.type = singer;
+}
+singerAbility.prototype = newAbility();
+
+///// ACTOR ABILITIES/////
 let badJokes = function() {
 	this.ability = "Bad Jokes";
 	this.damage = 8;
 };
-
-badJokes.prototype = new newAbility();
+badJokes.prototype = new actorAbility();
 
 let throwShade = function() {
 	this.ability = "Throw Shade";
 	this.damage	= 8;	
 };
-
-throwShade.prototype = new newAbility();
+throwShade.prototype = new actorAbility();
 
 let passiveAgg = function() {
 	this.ability = "Passive Aggresion";
 	this.damage	= 8;	
 };
+passiveAgg.prototype = new actorAbility();
 
-passiveAgg.prototype = new newAbility();
 
+/////SINGER ABILITIES/////////
 let opera = function() {
 	this.ability = "Opera";
 	this.damage	= 8;	
 };
-
-opera.prototype = new newAbility();
+opera.prototype = new singerAbility();
 
 let sonicScreech = function() {
 	this.ability = "Sonic Screech";
 	this.damage	= 8;	
 };
-
-sonicScreech.prototype = new newAbility();
-
-let cuteness = function() {
-	this.ability = "Cuteness";
-	this.damage	= 8;	
-};
-
-cuteness.prototype = new newAbility();
+sonicScreech.prototype = new singerAbility();
 
 let dancing = function() {
 	this.ability = "Dancing";
 	this.damage	= 8;	
 };
+dancing.prototype = new singerAbility();
 
-dancing.prototype = new newAbility();
 
+/////BETTY WHITE////////	
+let cuteness = function() {
+	this.ability = "Cuteness";
+	this.damage	= 15;	
+};
+cuteness.prototype = new newAbility();
+
+/////KANYE WEST////////
 let smugness = function() {
 	this.ability = "Smugness";
-	this.damage	= 8;	
+	this.damage	= 2;	
 };
-
 smugness.prototype = new newAbility();
-
 
 
 module.exports = {
